b19553598f8573ce5f40772dbc9cacab57eb4dc7
Alexey Zotov <alexey1.zotov@yandex.ru>
2026-05-25 18:56:04 +0300

    Init commit

--- main.go
+++ main.go
@@ -1,31 +1,38 @@
 package main
 
 import (
+	"fmt"
 	"sync"
 )
 
 func ping(ch1 chan int, ch2 chan int, wg *sync.WaitGroup) {
 	defer wg.Done()
-	for i := 0; i < 20; i++ {
+	for i := 0; i < 5; i++ {
 		v, ok := <-ch1
+		fmt.Printf("ping received: %d ok:%t\n", v, ok)
 		if !ok {
 			return
 		}
 		v++
 		ch2 <- v
+		fmt.Printf("ping sent: %d ok:%t\n", v, ok)
 	}
 	close(ch2)
 }
 
 func pong(ch2 chan int, ch1 chan int, wg *sync.WaitGroup) {
 	defer wg.Done()
-	v, ok := <-ch2
-	if !ok {
-		close(ch1)
-		return
+	for {
+		v, ok := <-ch2
+		fmt.Printf("pong received: %d ok:%t\n", v, ok)
+		if !ok {
+			close(ch1)
+			return
+		}
+		v++
+		ch1 <- v
+		fmt.Printf("pong sent: %d ok:%t\n", v, ok)
 	}
-	v++
-	ch1 <- v
 }
 
 func main() {
