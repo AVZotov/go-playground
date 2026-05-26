4241c60171bdbbf6e8079fead154fea1798f2de6
Alexey Zotov <alexey1.zotov@yandex.ru>
2026-05-26 17:40:42 +0300

    ctx

--- main.go
+++ main.go
@@ -4,35 +4,56 @@ import (
 	"context"
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
 )
 
-func fetchData(ctx context.Context, url string) (string, error) {
-	res := make(chan string, 1)
-
-	go func() {
-		opt := []int{1, 4}
-		t := opt[rand.Intn(2)]
-		time.Sleep(time.Duration(t) * time.Second)
-		res <- fmt.Sprintf("Sending request to: %s\ntime elapsed: %d", url, t)
-	}()
-
-	select {
-	case v := <-res:
-		return v, nil
-	case <-ctx.Done():
-		return "", ctx.Err()
+func gen(ctx context.Context, timeOut time.Duration, in chan<- int) {
+	for {
+		v := rand.Intn(100)
+		select {
+		case in <- v:
+			time.Sleep(timeOut)
+		case <-ctx.Done():
+			return
+		}
 	}
 }
 
-func main() {
-	for i := 0; i < 10; i++ {
-		ctx, close := context.WithTimeout(context.Background(), 2*time.Second)
-		defer close()
-		v, err := fetchData(ctx, "http://www.google.com")
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println(v)
+func funIn(ctx context.Context, in ...<-chan int) (out chan int) {
+	out = make(chan int)
+	wg := sync.WaitGroup{}
+
+	for _, ch := range in {
+		wg.Add(1)
+		go func(<-chan int) {
+			defer wg.Done()
+			for v := range ch {
+				select {
+				case out <- v:
+				case <-ctx.Done():
+					return
+				}
+			}
+		}(ch)
+		go func() {
+			wg.Wait()
+			close(out)
+		}()
 	}
+	return out
+}
+
+func main() {
+
+	in := make(chan int)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*2)
+	defer cancel()
+
+	d := []int{100, 200, 300}
+	for _, dd := range d {
+		go gen(ctx, time.Duration(dd), in)
+	}
+	out := funIn(ctx, in)
+	fmt.Println(<-out)
 }
