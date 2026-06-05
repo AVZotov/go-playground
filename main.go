693127377350f2dd202ab4e7054f7c7a0cb414d3
Alexey Zotov <alexey1.zotov@yandex.ru>
2026-06-05 14:53:37 +0300

    PubSub

--- main.go
+++ main.go
@@ -2,39 +2,40 @@ package main
 
 import (
 	"fmt"
-	
+
 	"github.com/google/uuid"
 )
 
 type EventBus struct {
-	clients map[string]*HandlerWrapper
+	clients map[string][]*HandlerWrapper
 }
 
 func NewEventBus() *EventBus {
 	return &EventBus{
-		clients: make(map[string]*HandlerWrapper),
+		clients: make(map[string][]*HandlerWrapper),
 	}
 }
 
 func (eb *EventBus) Publish(topic string, data any) {
-	for _, v := range eb.clients {
-		if topic == v.topic {
-			v.Handler(data)
-		}
+	for _, hw := range eb.clients[topic] {
+		hw.Handler(data)
 	}
 }
 
 func (eb *EventBus) Subscribe(topic string, hw *HandlerWrapper) {
 	hw.topic = topic
-	eb.clients[topic] = hw
+	eb.clients[topic] = append(eb.clients[topic], hw)
 }
 
 func (eb *EventBus) Unsubscribe(topic string, hw *HandlerWrapper) {
-	for k, v := range eb.clients {
-		if topic == v.topic && k == hw.id {
-			delete(eb.clients, k)
+	handlers := eb.clients[topic]
+	updated := make([]*HandlerWrapper, 0, len(handlers))
+	for _, h := range handlers {
+		if h.id != hw.id {
+			updated = append(updated, h)
 		}
 	}
+	eb.clients[topic] = updated
 }
 
 type HandlerWrapper struct {
@@ -52,12 +53,12 @@ func NewHandlerWrapper(f func(data any)) *HandlerWrapper {
 
 func main() {
 	bus := NewEventBus()
-	
+
 	f1 := func(data any) { fmt.Println("h1 got:", data) }
 	f2 := func(data any) { fmt.Println("h2 got:", data) }
 	h1 := NewHandlerWrapper(f1)
 	h2 := NewHandlerWrapper(f2)
-	
+
 	bus.Subscribe("user.created", h1)
 	bus.Subscribe("user.created", h2)
 	bus.Subscribe("order.placed", h1)
